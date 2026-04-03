99add2f8f2557f76a6ef2e3119f74df936ddd2e4
Kashi-S-A <kashappaappannavar@gmail.com>
2026-04-03 16:23:40 +0530

    crud and resultset

--- 01-JDBC-CRUD/src/main/java/com/ty/crud/Save.java
+++ 01-JDBC-CRUD/src/main/java/com/ty/crud/Save.java
@@ -25,10 +25,10 @@ public class Save {
 			Statement stm = con.createStatement();
 			System.out.println("Statement created");
 
-			String query = "INSERT INTO student VALUES(102,'Pengi','pengi@gmail.com',45678)";
+			String query = "INSERT INTO student VALUES(103,'Manga','manga@gmail.com',87657)";
 
 			// step 4 : Execute the query
-			stm.execute(query);
+			System.out.println(stm.execute(query));
 			System.out.println("record saved");
 
 			// step 5 : Close the connection
